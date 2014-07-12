d835ab9374fb15f417c73530182475ebda6b6bea
Vladimir Panteleev <vladimir@thecybershadow.net>
2014-07-13 01:20:19 +0300

    Allow using ae/openssl instead of curl

--- query.d
+++ query.d
@@ -1,26 +1,69 @@
-import std.net.curl;
+import core.thread;
 import std.json;
 import std.stdio;
 import std.file;
 import std.string;
-import core.thread;
+
+version (AE)
+{
+	import ae.net.asockets;
+	import ae.net.http.client;
+	import ae.net.ssl.openssl;
+
+	string httpGet(string url)
+	{
+		auto request = new HttpRequest;
+		request.resource = url;
+
+		enum tokenFileName = "token.txt";
+		if (tokenFileName.exists)
+			request.headers["Authorization"] = "token " ~ readText(tokenFileName).strip();
+
+		string result;
+		httpRequest(request,
+			(Data data)
+			{
+				result = (cast(char[])data.contents).idup;
+				std.utf.validate(result);
+			},
+			(string error) { throw new Exception(error); }
+		);
+
+		socketManager.loop();
+
+		return result;
+	}
+}
+else
+{
+	import std.net.curl;
+
+	HTTP http;
+	static this()
+	{
+		http = HTTP();
+		http.verifyPeer = false;
+
+		enum tokenFileName = "token.txt";
+		if (tokenFileName.exists)
+			http.addRequestHeader("Authorization", "token " ~ readText(tokenFileName).strip());
+	}
+
+	char[] httpGet(string url)
+	{
+		return get(url, http);
+	}
+}
 
 void main()
 {
-	auto http = HTTP();
-	http.verifyPeer = false;
-
-	enum tokenFileName = "token.txt";
-	if (tokenFileName.exists)
-		http.addRequestHeader("Authorization", "token " ~ readText(tokenFileName).strip());
-
-	auto repos = get("https://api.github.com/users/DerelictOrg/repos", http).parseJSON().array;
+	auto repos = httpGet("https://api.github.com/users/DerelictOrg/repos").parseJSON().array;
 	foreach (repo; repos)
 	{
 		auto name = repo["name"].str;
 		//writeln(name, ":");
 		Thread.sleep(1.seconds);
-		auto packages = get("https://api.github.com/repos/DerelictOrg/" ~ name ~ "/contents/source/derelict/", http).parseJSON().array;
+		auto packages = httpGet("https://api.github.com/repos/DerelictOrg/" ~ name ~ "/contents/source/derelict/").parseJSON().array;
 		auto p = packages[0]["name"].str;
 		//foreach (p; packages)
 		//	writeln(p["name"].str);
